66e0db1ca971a4c154f986c54f893b270c9efbbe
Vincent Truche <vince.truche@gmail.com>
2022-04-29 10:49:32 +0200

    Correction exercice 4

--- exo3/src/index.js
+++ exo3/src/index.js
@@ -1,5 +1,18 @@
+// Modules préents de bases dans nodejs, pas besoin de les npm i
+const path = require('path');
 const fs = require('fs');
 
-let output = fs.readFileSync('src/log.txt', 'utf-8');
-console.log(output);
+// Ici je trouve les chemins absolus DONE
+const cheminAbsoluIndexHtml = path.join(__dirname ,'../assets/index.html');
+const cheminAbsoluLogTxt = path.join(__dirname , 'log.txt');
+
+console.log(cheminAbsoluIndexHtml);
+console.log(cheminAbsoluLogTxt);
+
+// Ici je les utilise pour lire les fichiers
+const contenuIndexHtml = fs.readFileSync(cheminAbsoluIndexHtml, "utf-8");
+const contenuLogTxt = String(fs.readFileSync(cheminAbsoluLogTxt));
+
+console.log(contenuIndexHtml);
+console.log(contenuLogTxt);
 
